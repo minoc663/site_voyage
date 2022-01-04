5b20875e803d1d5795837addbf754e381be9da13
Le Thi My Ngoc <myngocle99@gmail.com>
2022-01-04 17:01:36 +0100

    vol
    controller + form

--- src/Controller/VolController.php
+++ src/Controller/VolController.php
@@ -2,17 +2,42 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Vol;
+use App\Form\VolType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class VolController extends AbstractController
 {
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
     /**
      * @Route("/vol", name="vol")
      */
-    public function vol(): Response
+    public function vol(Request $request): Response
     {
-        return $this->render('vol/vol.html.twig',);
+
+        $vol = new Vol();
+
+
+        $form = $this->createForm(VolType::class, $vol);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $vol = $form->getData();
+
+            $this->entityManager->persist($vol);
+            $this->entityManager->flush();
+        }
+
+        return $this->render('vol/vol.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
--- src/Form/VolType.php
+++ src/Form/VolType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Vol;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,16 +14,31 @@ class VolType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('compagnie')
-            ->add('date_depart')
-            ->add('date_arrivee')
+            ->add('compagnie', TextType::class, [
+                'label' => 'Le nom de la compagnie',
+                'attr' => [
+                    'placeholder' => 'Veuillez saisir le nom de la compagnie'
+                ]
+            ])
+            ->add('date_depart', DateTimeType::class, [
+                'label' => 'date de départ',
+
+            ])
+
+            ->add('date_arrivee', DateTimeType::class, [
+                'placeholder'
+                => [
+                    'year' => 'Year', 'month' => 'Month', 'day' => 'Day'
+
+                ],
+            ])
+
             ->add('lieu_depart')
             ->add('lieu_arrivee')
             ->add('prix')
             ->add('duree_vol')
             ->add('nombre_place')
-            ->add('date_retour')
-        ;
+            ->add('date_retour');
     }
 
     public function configureOptions(OptionsResolver $resolver): void
