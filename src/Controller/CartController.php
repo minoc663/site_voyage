dcb2c90f13ed0ee0fee8e8af0f4a7f8dcdb8e084
alexia-rnd <alexia.rnd@gmail.com>
2022-01-07 12:32:35 +0100

    mon panier

---
+++ src/Controller/CartController.php
@@ -0,0 +1,67 @@
+<?php
+
+
+
+namespace App\Controller;
+
+use App\Cart;
+use App\Entity\Vol;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CartController extends AbstractController
+{
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/mon-panier", name="cart")
+     */
+    public function index(Cart $cart)
+    {
+        $cartComplete =[];
+
+        foreach ($cart->get() as $id => $quantity){
+            $cartComplete[] = [
+                'vol' => $this->entityManager->getRepository(Vol::class)->findOneBy($id),
+                'quantity' => $quantity
+            ];
+        }
+
+        return $this->render('cart/cart.html.twig', [
+            'cart' => $cartComplete
+        ]);
+    
+    }
+
+    /**
+     * @Route("/cart/add/{id}", name="add_to_cart")
+     */
+    public function add(Cart $cart, $id)
+    {
+        $cart->add($id);
+        
+        return $this->redirectToRoute('cart');
+    
+    }
+
+    //REMOVE
+    /**
+     * @Route("/cart/remove", name="remove_my_cart")
+     */
+    public function remove(Cart $cart)
+    {
+        $cart->remove();
+        return $this->redirectToRoute('home');
+    
+    }
+}
+
+
